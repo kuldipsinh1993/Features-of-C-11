e7fadcd5ac82b084dab7821b1d9dbad38dedd52f
kuldipsinh1993 <46772562+kuldipsinh1993@users.noreply.github.com>
2020-01-11 09:31:27 +0530

    Update 1.cpp

--- 26thDec/lambdas/1.cpp
+++ 26thDec/lambdas/1.cpp
@@ -13,14 +13,20 @@ using namespace std;
 
 int main()
 {
-	vector<int> v{1,2,3,4};
-	for(v.begin(), v.end(), [](int i){ cout << i << " "; }
-	cout << endl;
-	    
-	// function to count numbers greater than or equal to 5 
-    	int count_5 = count_if(v.begin(), v.end(), [](int a) 
-    			{ 
-    	    			return (a >= 5); 
-    			}); 
+	vector<int> v{1,21,32,4};
+        for_each(v.begin(), v.end(), [](int i)
+        {
+                cout << i << " ";
+        });
+
+        cout << endl;
+
+        // function to count numbers greater than or equal to 5 
+        int count_5 = count_if(v.begin(), v.end(), [](int a)
+                        {
+                                return (a >= 5);
+                        });
+
+        cout << "Count_5 : " << count_5 << endl;
 	return 0;
 }
